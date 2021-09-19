e835e65ce847adb201f5e7239e4a2a0edbb5f902
cukmekerb <cukmekerb@gmail.com>
2021-09-18 23:04:46 -0700

    basic upgrade buying/selling system and animations and stuff

--- clicker.js
+++ clicker.js
@@ -2,7 +2,7 @@ var framespeed = 50; // how many ms in between each game loop
 var default_format = {
 	sigfigs: 4
 };
-
+var loaded = false;
 var game = {
 	oat_count: 200,
 	ops: 0,
@@ -77,7 +77,10 @@ var game = {
 			customfunc: {
 				run_every: 60, // interval (seconds) to run this at
 				run: owned => {
-					// TODO: cinnamon function
+					if (Math.random() * owned > 100) {
+						// TODO: win the lottery
+						console.log("cinnamon win")
+					}
 				}
 			},
 			type: "upgrade"
@@ -86,11 +89,22 @@ var game = {
 };
 
 function game_tick() {
-	
+	for (let i in game.upgrades) {
+		if (game.oat_count >= game.upgrades[i].price) {
+			document.querySelector("#price-" + i).classList.remove("not-enough-money");
+			document.querySelector("#price-" + i).classList.add("enough-money");
+			document.querySelector("#booster-" + i).classList.remove("cantbuy");
+		}
+		else {
+			document.querySelector("#price-" + i).classList.remove("enough-money");
+			document.querySelector("#price-" + i).classList.add("not-enough-money");
+			document.querySelector("#booster-" + i).classList.add("cantbuy");
+		}
+	}
 }
 
 async function init() {
-	console.log("test")
+	console.log("%cEH!\n%cNot here to cheat, are we? \nIf you truly are here for debugging, please report any issues you find at %chttps://github.com/ooogle/oatmeal2/issues/new", "color:blue; font-size: 100px;", "color:#0068df; font-size: 15px;", "font-size:14px; color:#0276fc; background:lightgray;");
 	
 	let booster_template = await fetch("/templates/booster_template.hbs").then(a => a.text());
 	
@@ -100,10 +114,28 @@ async function init() {
 			...game.upgrades[i],
 			can_afford: game.oat_count >= game.upgrades[i].price,
 			display_price: numberformat.format(game.upgrades[i].price, default_format),
-			display_count: numberformat.format(game.upgrades[i].owned, default_format)
+			display_count: numberformat.format(game.upgrades[i].owned, default_format),
+			id: i
 		}
 		let target_element = "#" + game.upgrades[i].type + "s";
 		fill_template(booster_template, template_data, target_element);
 	}
+}
 
+function update_prices() {
+	for (let i in game.upgrades) {
+		game.upgrades[i].price = game.upgrades[i].base_price * (1 + game.upgrades[i].price_interest) ** game.upgrades[i].owned
+		document.querySelector("#price-" + i).innerHTML = numberformat.format(game.upgrades[i].price, default_format);
+		document.querySelector("#owned-" + i).innerHTML = numberformat.format(game.upgrades[i].owned, default_format);
+	}
+}
+
+function buy_upgrade(upgrade) {
+	if (game.oat_count < game.upgrades[upgrade].price) return;
+	game.upgrades[upgrade].owned++;
+	if (game.upgrades[upgrade].multiplies) {
+		game.upgrades[game.upgrades[upgrade].multiplies].multiplier += game.upgrades[upgrade].multiplier;
+	}
+	game.oat_count -= game.upgrades[upgrade].price;
+	update_prices();
 }
