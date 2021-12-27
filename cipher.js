1f07c3e061d9524180daa7c4804310a39213d726
cukmekerb <cukmekerb@gmail.com>
2021-12-27 14:35:19 -0800

    rewrite cipher (I am a genius)

--- cipher.js
+++ cipher.js
@@ -1,51 +1,77 @@
+// this works like base64 but different. idk I followed a base64 tutorial as a "basis" but it came out wrong. still works though
 (() => {
-	let alphabet = "plank4ouchOUCHthIsOTEZwWkKPAN_#7^9I{}[]|()&*%1:;\"'><?/,.!\\@2356-+=~`";
-	let splitter = "$";
-	let inverse_alpha = {}
+	// stolen from https://stackoverflow.com/a/37826698/15317442
+	function splitarray(array, chunksize) {
+		return array.reduce((result, item, index) => { 
+			const chunk_index = Math.floor(index / chunksize)
+			
+			if(!result[chunk_index]) {
+				result[chunk_index] = []; // start a new chunk
+			}
+			
+			result[chunk_index].push(item)
+			
+			return result
+		}, []);
+	}
 	
-	for (let i = 0; i < alphabet.length; i++) {
-		inverse_alpha[alphabet[i]] = i;
+	function string_to_binary(string) {
+		let binary = [];
+		for (let i = 0; i < string.length; i++) {
+			binary.push(string[i].charCodeAt(0).toString(2));
+		}
+		return binary;
+	}
+	
+	function binary_to_string(binary) {
+		let string = "";
+		for (let i in binary) {
+			string += String.fromCharCode(parseInt(binary[i], 2));
+		}
+		return string;
 	}
 	
 	function encode_string(str) {
-		let chars = [];
+		str = encodeURIComponent(str);
 		let newstr = "";
-		for (let i in str) {
-			chars[i] = str[i].charCodeAt();
+		let bytes = string_to_binary(str);
+		
+		for (let i in bytes) {
+			bytes[i] = "0".repeat(8 - bytes[i].length) + bytes[i]; // make sure all bytes are byte-sized
 		}
 		
-		function encodechar(char) {
-			if (char < alphabet.length) {
-				return alphabet[char];
-			}
-			let char1 = Math.floor(char / 2) + char % 2;
-			let char2 = char - char1;
-			return encodechar(char1) + encodechar(char2)
+		let bits = bytes.join("");
+		let groups = splitarray(bits.split(""), 6); // split binary into 6-bit groups
+				
+		for (let i in groups) {
+			groups[i] = groups[i].join("");
+			newstr += String.fromCharCode(parseInt(groups[i], 2) + 60); // convert to string, with the character being 60 after the group's value
 		}
 		
-		for (let char of chars) {
-			newstr += encodechar(char) + splitter;
-		}
 		return newstr;
 	}
 	
 	function decode_string(str) {
-		let chars = str.split(splitter);
-		chars.pop();
 		let newstr = "";
+		let groups = [];
 		
-		function decodechar(char) {
-			let ints = char.split("");
-			let charcode = 0;
-			for (let int of ints) {
-				charcode += inverse_alpha[int];
-			}
-			return String.fromCharCode(charcode);
+		for (let i = 0; i < str.length; i++) {
+			groups.push((str[i].charCodeAt(0) - 60).toString(2)); // convert each character back to a 6-bit group
 		}
 		
-		for (let char of chars) {
-			newstr += decodechar(char);
+		for (let i = 0; i < groups.length - 1; i++) {
+			groups[i] = "0".repeat(6 - groups[i].length) + groups[i]; // pad groups
 		}
+				
+		
+		let bytes = splitarray(groups.join("").split(""), 8); // convert groups of 6 to groups of 8
+		
+		for (let i in bytes) {
+			bytes[i] = bytes[i].join("");
+		}
+		
+		newstr = binary_to_string(bytes);
+		newstr = decodeURIComponent(newstr);
 		return newstr;
 	}
 	window.cipher = {
